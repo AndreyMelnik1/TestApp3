cd72d477796c8cd473f9715df42c92075ed5f660
Андрей Мельник <andrejmelnik@MacBook-Pro-Andrej.local>
2022-03-15 22:45:47 +0300

    first commit

---
+++ TestApp3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestApp3
+//
+//  Created by Андрей Мельник on 15.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
